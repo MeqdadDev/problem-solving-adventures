38ff3c35b00352a3321b754c5d480a9d3af08eed
MeqdadDev <meqdad.darweesh@gmail.com>
2023-08-25 22:00:28 +0300

    add .gitignore file

--- Blind-75-Questions/3.Contains-Duplicate/Sol-1.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        unique_numbers = set(nums)
-        if len(unique_numbers) != len(nums):
-            return True
-        else:
-            return False
--- Blind-75-Questions/3.Contains-Duplicate/Sol-2.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hashmap = dict()
-        for n in nums:
-            if n in hashmap:
-                return True
-            else:
-                hashmap[n] = n
-        return False
