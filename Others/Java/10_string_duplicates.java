3d576fd09c66f35434cc3b0949e61693200fdffd
MeqdadDev <meqdad.darweesh@gmail.com>
2024-02-09 14:32:04 +0200

    update string duplicates sol.1 in java

--- Others/Java/10_string_duplicates.java
+++ Others/Java/10_string_duplicates.java
@@ -3,14 +3,11 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static void findDuplicates(String s){
+    public static void findDuplicates_Sol1(String s){
         char[] chars = s.toCharArray();
 
-        // Sort the character array
         Arrays.sort(chars);
 
-        // Iterate through the sorted array to find duplicates
-        System.out.println("Duplicate characters in the string '" + s + "':");
         for (int i = 0; i < chars.length - 1; i++) {
             if (chars[i] == chars[i + 1]) {
                 System.out.println(chars[i]);
